ba3a80cbb33069ca1d070b57dca1f6c25538284d
lianhuawuzhe <3518926591@qq.com>
2024-07-17 11:29:30 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	//int a =(int) 3.14;
+//	//printf("%d", a);
+//	int a = 10;
+//	if (a == 3)
+//	{
+//		printf("hi");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+	/*int a = 0;
+	int b = 10;
+	if (a && b)
+	{
+		printf("hi");
+	}*/
+	/*int a = 10;
+	int b = 0;
+	if (a || b)
+	{
+		printf("hi");
+	}*/
+	/*int a = 10;
+	int b = 20;
+	int r = a > b ? a : b;
+	printf("%d", b);*/
+	/*int a = 10;
+	int b = 20;
+	int c = 0;
+	int d = (c = a - 2, a = b + c, c - 3);
+	printf(" % d", d);*/
+	/*int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d", arr[3]);*/
+
+
+
+	//return 0;
+//
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int main() 
+//{
+//	int sum=Add(2, 3);
+//	printf("%d",sum);
+//	return 0;
+//
+//}
+
+//#define NUM 100
+//#define Add(x,y)x+y
+//int main()
+//{
+	/*printf("%d\n", NUM);
+	int n = NUM;
+	printf("%d\n", n);
+	int arr[NUM] = { 0 };*/
+	//int a = 10;
+	//int b = 20;
+	//int c = Add(a, b);
+	//printf("%d\n", c);
+
+	//return 0;
+//}
+int main()
+{
+	int a = 10;
+	//	&a;
+	printf("%p", &a);
+	int* p = &a;
+	return 0;
+}

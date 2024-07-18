5a566d8738acc3331808aca3ad7287f1f6d8229b
lianhuawuzhe <3518926591@qq.com>
2024-07-18 10:30:06 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//int main()
+//{
+//	//int arr[10] = { 10,11,12,13,14,15,16,17,18,19 };
+//	char ch[5] = { 'a','b','c','d','e' };
+//	//double d[30];
+//	/*printf("%d\n", arr[8]);*/
+//	//int i = 0;
+//	//while (i< 10)
+//	//{
+//		//printf("%d\n", arr[i]);
+//	
+//		//i = i + 1;
+//
+//	//}
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		printf("%c\n", ch[i]);
+//		i = i + 1;
+//	}
+//
+//	return 0;
+//}
+
+//int Add
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	return 0;
+//	int c = Add(a, b);
+//}
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z= x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d", c);
+//	return 0;
+////}
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+
+//int main()
+//{
+	//int arr[10] = { 1 };
+	//int n = 10;
+	//int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	//int a = 0;
+	//int b = 0;
+	//scanf("%d %d", &a,& b);
+	//int r=Max(a, b);
+	//printf("%d", r);
+
+	//return 0;
+//}
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d", &x);
+//	if (x > 0)
+//		y = -1;
+//	else if (x == 0)
+//		y = 0;
+//	else
+//		y = 1;
+//	printf("%d", y);
+//
+//	return 0;
+//}

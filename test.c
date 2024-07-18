5ffd66b70c3cf7a783246801ea6f9644f70b60a6
lianhuawuzhe <3518926591@qq.com>
2024-07-18 10:27:44 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int main()
+//{
+	//short age = 20;
+	//int hhigh = 180;
+	//float weight = 88.5;
+    //return 0;
+//}
+//
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//
+//	scanf("%d %d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//}
+int main()
+{
+	int a = 10;
+	{
+
+		printf("a=%d\n", a);
+	}
+
+	printf("a=%d\n", a);
+	return 0;
+}

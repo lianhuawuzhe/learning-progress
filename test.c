15d1f901437c21eb8a4e4a153ae4e5b4c8ecb9c1
lianhuawuzhe <3518926591@qq.com>
2024-07-18 10:23:08 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int a = 0;
+//	printf("要好好学习吗(1/0)");
+//	scanf("%d", &a);
+//	if (a == 1)
+//	{
+//
+//
+//		printf("好");
+//	}
+//	else
+//	{
+//		printf("不好");
+//	}
+//
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("加入爷\n");
+//
+//	while (line<20000)
+//	{
+//		printf("写代码;%d\n",line);
+//		line++;
+//	}
+//	if (line == 20000) {
+//		printf("offer\n");
+//	}
+//	else
+//	{
+//		printf("loser\n");
+//	}
+//	return 0;
+
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+	//int n1 = 0;
+	//int n2 = 0;
+
+	//scanf("%d %d", &n1, &n2);
+	////int sum= n1 + n2;
+	//int sum = Add(n1, n2);
+	//int t1 = 100;
+	//int t2 = 200;
+	//int ret = Add(t1, t2);
+	//printf("%d\n",sum);
+//}
+
+// printf("写代码");
+//line++; printf("写代码");
+//line++; printf("写代码");
+//line++printf("写代码");
+//line++; printf("写代码");
+//line++;v
+
+
+
+int main()
+{
+	//int a = 10;
+	//int b = 11;
+	//int c = 13;
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+
+
+	return 0;
+}

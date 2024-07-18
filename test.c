3ed5fa79e6c3f30b19772d4f708700d5557cfe5e
lianhuawuzhe <3518926591@qq.com>
2024-07-18 10:29:00 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,90 @@
+#define _ CRT_ SECURE _NO_ WARNINGS
+#include<stdio.h>
+
+//extern int a;
+//
+//int main()
+//{
+//	printf("%d=a", a);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	{
+//		int a = 10;
+//		printf("%d\n", a);
+//
+//	}
+//	return 0;
+//}
+//int main()
+// 
+// 
+// 
+
+
+// //	//	printf("%d=1");
+// 
+
+
+
+
+
+//#define MAX 100
+//#define STR "abcdef" 
+//int main()
+//{
+	//const int n = 10;
+	//int arr[n] = { 0 };
+	//printf("%d\n", MAX);
+	//int a = MAX;
+	//printf("%d\n", a);
+	//printf("%s\n", STR);
+//	MAX = 200;
+//
+//
+//
+//	return 0;
+//}
+
+
+
+//	return 0;
+//}
+//enum Color{}
+//int main()
+//{
+//
+//	
+//	char arr[] = "abcdef";
+//	return 0;
+//}
+
+#include <string.h>
+
+int main()
+{
+	//char arr1[] = "abcdef";
+	//char arr2[] = { 'a','b','c','d','e','f','\0'};
+
+	//printf("%s\n", arr1);
+	//printf("%s\n", arr2);
+	//printf("%d\n", strlen(arr1));
+
+	//printf("%d\n", strlen(arr2));
+
+	/*printf("abc\n");*/
+//printf("abc\0ef");
+	//printf("%s\n","(are you ok??)");
+	//printf("%c\n",'\'');
+	//printf("\"");
+	//printf("abc\\0ef");
+	//printf("c:\\test\\test.c");
+	printf("\a");
+	return 0;
+
+}
+
+
+

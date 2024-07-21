2273b9608a1c89ccfa09ad2a03042bd567a29ae0
lianhuawuzhe <3518926591@qq.com>
2024-07-21 09:12:43 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,148 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			//break;
+//			continue;
+//		printf("%d\n", i);
+//		i = 12;
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	/*int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}*/
+//	int i = 100;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		printf("%d", i);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+	/*for (; ;)
+		printf("hi");*/
+	/*int i = 0;
+	int j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("hi\n");
+		}
+	}*/
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("hi\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		//i++;
+//		if (i == 5)
+//			//break;
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	} 
+//	while (i <= 10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d", ret);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		int ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int n = 1;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 }; 
+//	int i = 0;
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (arr[i] == k)
+//		{
+//			printf("找到了，下标是:%d\n",i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到");
+//
+//	return 0;
+//}
+

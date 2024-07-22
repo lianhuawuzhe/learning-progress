32efc24c1d29003dd8aa9ebc0edf6620a4d848f9
lianhuawuzhe <3518926591@qq.com>
2024-07-22 17:10:54 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<string.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<=right)
+//	{
+//		//int mid = (left + right) / 2;
+//		int mid = left + (right - left) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{	printf("找到了，下标是是:%d\n", mid);
+//			break;
+//		}
+//
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到");
+//	}
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "welcome to me";
+//	char arr2[] = "#############";
+//	int left = 0;
+//	int right = sizeof(arr1) / sizeof(arr1[0]) - 2;
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);
+//		system("cls");
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = {0};
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s", password);
+//		if (strcmp(password , "abcdef")==0)
+//		{
+//			printf("登录成功\n");
+//			break;
+//		}
+//		else
+//			printf("密码错误\n");
+//	}
+//	if (i == 3)
+//	{
+//		printf("退出程序\n");
+//	}
+//	return 0;
+//}

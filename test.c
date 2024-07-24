bc1a00149099cf317dec4857dd7c3c76055db9ca
lianhuawuzhe <3518926591@qq.com>
2024-07-24 22:30:35 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//again:
+//	printf("hehe\n");
+//	printf("haha\n");
+//	goto again;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+////again:
+//	while (1)
+//	{
+//		printf("如果输入:我是猪，可取消关机\n");
+//		scanf("%s", input);
+//		if (strcmp(input, "我是猪") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	//else
+//	//{
+//	//	goto again;
+//	//}
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}

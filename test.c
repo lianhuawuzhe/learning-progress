db1cbee76f23d22f3566815a9262d5f1dc0b983b
lianhuawuzhe <3518926591@qq.com>
2024-07-26 23:32:40 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	char arr[20] = "hello woorld";
+//	memset(arr+6,'x',3);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int get_max(int x,int y)
+//{
+//	return (x > y ? x : y);
+//}
+//
+//void test()
+//{
+//	printf("hihi\n");
+//}
+//int main()
+//{
+//	test();
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int m=get_max(a, b);
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+//
+//void Swap(int x,int y)  //错误代码
+//{
+//	int z = 0;
+//	z = x;
+//	x = y;
+//	y = z;
+//}
+
+
+//void Swap(int *px, int *py)  //错误代码
+//{
+//	int z = *px;
+//	*px = *py;
+//	*py = z;
+//	
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("交换前:a=%d,b=%d\n", b, a);
+//	Swap(&a, &b);
+//	printf("交换后:a=%d,b=%d\n", b, a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	a = 20;
+//	*p = 30;
+//	printf("%d\n", a);
+//	return 0;
+//
+
+int main()
+{
+
+	return 0;
+}

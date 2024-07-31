d5636f60ce0f08ff1593f351347a091b4fda9669
lianhuawuzhe <3518926591@qq.com>
2024-07-31 22:11:46 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 ==9)
+//		{
+//			a++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			a++;
+//		}
+//	}
+//	printf("%d", a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum = sum + (1.0 / i) * flag;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int j = 0;
+//	for (j = 0; j < 10; j++)
+//	{
+//		scanf("%d", &arr[j]);
+//	}
+//	int i = 0;
+//	int max = arr[0];
+//	for (i = 1; i <= 9; i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	int res = 0;
+//	for (m = 1; m <= 9; m++)
+//	{
+//		for (n = 1; n <= 9; n++)
+//		{
+//			res = m * n;
+//			printf("%d*%d=%d ",m,n,res);
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	int res = 0;
+//	for (m = 1; m <= 9; m++)
+//	{
+//		for (n = 1; n <= m; n++)
+//		{
+//			res = m * n;
+//			printf("%d*%d=%-2d ", m, n, res);
+//		}
+//		printf("\n");
+//
+//	}
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}

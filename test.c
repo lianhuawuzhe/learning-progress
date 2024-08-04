e865bedbbc678fa73b9b8274fb42579c986a0e0d
lianhuawuzhe <3518926591@qq.com>
2024-08-04 21:32:13 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	//int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	//int right = sz - 2;
+//	int right = strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		char tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//
+//		left++;
+//		right--;
+//
+//	}
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+int main()
+{
+	char arr[] = "abcdef";
+	(arreverser);
+	printf("%s\n", arr);
+
+	return 0;
+}

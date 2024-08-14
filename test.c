42c98e7098dade26d7193fcacd11efd462c7f3f7
lianhuawuzhe <3518926591@qq.com>
2024-08-14 22:12:33 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int money = 0;
+	scanf("%d", &money);
+	int total = money;
+	int empty = money;
+	while (empty >= 2)
+	{
+		total += empty / 2;
+		empty=empty/2+e'm'
+	}
+	return 0;
+}

4e379f88a9a53cc36765cf49128c2892097a5c03
lianhuawuzhe <3518926591@qq.com>
2024-09-04 21:07:48 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//double count(int n)
+//{
+//	if (n == 1) 
+//		return 1.0; 
+//
+//	return 1.0 / (count(n - 1) + 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	double ret = count(n);
+//	printf("%.3f", ret);
+//	
+//	return 0;
+//}
+
+
+//今天学的是行列式
